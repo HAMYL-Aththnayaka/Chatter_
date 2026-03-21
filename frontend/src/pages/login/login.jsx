b5a46a4f70fa8bc7c3eca5a1351eb96c56e20438
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-21 22:16:03 +0530

    login singip ready

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -5,6 +5,12 @@ const login =()=>{
     return(
     <div className='login'>
         <img src={Assets.logo_big} alt='' className='logo'/>
+        <form action='' className='login-form'>
+            <h2>Sign UP</h2>
+            <input type="text" placeholder='username'className="form-input" required />
+            <input type="email" placeholder='email ' className="form-input" />
+            <input type="password" placeholder='password' className="form-input" />
+        </form>
     </div>);
 }
 
