dab1fc38ea20e55b3d949898d1c60c6ea2d9ad34
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-21 23:44:19 +0530

    toggle logic added

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -3,25 +3,27 @@ import './login.css'
 import Assets from '../../assets/assets'
 import { useState } from "react";
 const login = () => {
-    const [currState,setCurrState]=useState("sign up");
+    const [currState, setCurrState] = useState("sign up");
     return (
         <div className='login'>
             <img src={Assets.logo_big} alt='' className='logo' />
             <form action='' className='login-form'>
                 <h2>{currState}</h2>
-                {(currState == 'sign up')?
-                <input type="text" placeholder='username' className="form-input" required />:"" 
+                {(currState == 'sign up') ?
+                    <input type="text" placeholder='username' className="form-input" required /> : ""
                 }
                 <input type="email" placeholder='email ' className="form-input" />
                 <input type="password" placeholder='password' className="form-input" />
-                <button type='submit'>Sign UP</button>
+                {currState == "sign up" ?
+                    <button type='submit'>Sign UP</button> : <button type='submit'>Log in</button>
+                }
                 <div className="login-term">
                     <input type='checkbox' />
                     <p>Agree to the terms of use & privacy policy.</p>
                 </div>
 
                 <div className='login-forgot'>
-                    <p className='login-toggle'>{(currState == "sign up")?<p>Already have an Account</p>:<p>Dont Have a Account </p>} <span onClick={()=>{(currState == "sign up")?setCurrState("Log in"):setCurrState('sign up')}}>click Here</span></p>
+                    <p className='login-toggle'>{(currState == "sign up") ? <p>Already have an Account</p> : <p>Dont Have a Account </p>} <span onClick={() => { (currState == "sign up") ? setCurrState("Log in") : setCurrState('sign up') }}>click Here</span></p>
                 </div>
             </form>
         </div>);
