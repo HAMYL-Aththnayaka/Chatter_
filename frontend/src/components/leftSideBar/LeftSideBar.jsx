b9aa624f037678328e207ab886c3b3bbfb11ac3d
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-22 21:17:34 +0530

    left side work started

--- frontend/src/components/leftSideBar/LeftSideBar.jsx
+++ frontend/src/components/leftSideBar/LeftSideBar.jsx
@@ -9,6 +9,13 @@ const LeftSideBar = () => {
       <div className="ls-top">
         <div className="ls-nav">
           <img src={assets.logo} alt="" className='logo' />
+            <div className="menu">
+              <img src={assets.menu_icon} alt='' />
+            </div>
+            <div className="ls-search">
+              <img src={assets.search_icon} alt=''/>
+              <input type='text' placeholder='search ....'/>
+            </div>
         </div>
       </div> 
     </div>
