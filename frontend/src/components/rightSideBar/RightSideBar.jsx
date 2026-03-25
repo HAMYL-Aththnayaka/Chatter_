a5471865ca12fa2281dae0941f5ddbe8fb021e85
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-25 14:20:21 +0530

    right side

--- frontend/src/components/rightSideBar/RightSideBar.jsx
+++ frontend/src/components/rightSideBar/RightSideBar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from "../../assets/assets"
+
 
 const RightSideBar = () => {
   return (
-    <div>
-      
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt=''/>
+        <h3>Mr Aththanayaka <img src={assets.green_dot} className='dot' alt=''/></h3>
+        <p>Why are You Running </p>
+      </div>
+      <hr/>
+      <div className="rs-media">
+        <p>media</p>
+        <div>
+          <img src={assets.pic1} alt=''/>
+          <img src={assets.pic2} alt=''/>
+          <img src={assets.pic3} alt=''/>
+          <img src={assets.pic4} alt=''/>
+          <img src={assets.pic1} alt=''/>
+          <img src={assets.pic2} alt=''/>
+        </div>
+      </div>
+        <button>Logout</button>
     </div>
   )
 }
