3ee0c427c7b5a27fd256ec30d7cb9a00c88d35c1
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-25 13:35:42 +0530

    bug was found

--- frontend/src/components/chatBox/chatBox.jsx
+++ frontend/src/components/chatBox/chatBox.jsx
@@ -2,35 +2,58 @@ import React from 'react'
 import './chatBox.css'
 import assets from '../../assets/assets'
 
-const chatBox = () => {
+const ChatBox = () => {
   return (
     <div className="chat-box">
+      {/* User Header */}
       <div className="chat-user">
-        <img src={assets.profile_img} alt=""/>
-        <p>Yasas Aththanayaka <img src={assets.green_dot} alt="" className='dot'/></p>
-        <img src={assets.help_icon} alt=""/>
+        <img src={assets.profile_img} alt="profile" />
+        <p>Yasas Aththanayaka <img src={assets.green_dot} alt="" className='dot' /></p>
+        <img src={assets.help_icon} alt="help" />
       </div>
 
-
+      {/* Message Area */}
       <div className="chat-msg">
+        
+        {/* Sent Text Message */}
         <div className="s-msg">
-          <p className="msg">The Typed Massage for display</p>
+          <img src={assets.profile_img} alt='sender-pp' />
+          <div>
+            <p className="msg">The Typed Message for display</p>
+            <p className="time">2:30</p>
+          </div>
         </div>
-        <img src={assets.profile_img} alt=''/>
-        <p>2:30</p>
+
+        {/* Sent IMAGE Message (The image you sent) */}
+        <div className="s-msg">
+          <img src={assets.profile_img} alt='sender-pp' />
+          <div>
+            <img className='msg-image' src={assets.pic1} alt='sent-pic' />
+            <p className="time">2:30</p>
+          </div>
+        </div>
+
+        {/* Received Text Message */}
+        <div className="r-msg">
+          <img src={assets.profile_img} alt='receiver-pp' />
+          <div>
+            <p className="msg">The Typed Message for display</p>
+            <p className="time">2:30</p>
+          </div>
+        </div>
       </div>
 
-
+      {/* Input Area */}
       <div className="chat-input">
-        <input type="text" placeholder="send a message"/>
-        <input type="file" id='image' accept='image/png,image/jpeg' hidden/>
+        <input type="text" placeholder="send a message" />
+        <input type="file" id='image' accept='image/png,image/jpeg' hidden />
         <label htmlFor='image'>
-          <img src={assets.gallery_icon} alt=''/>
+          <img src={assets.gallery_icon} alt='gallery' />
         </label>
-        <img src={assets.send_button} alt=''/>
+        <img src={assets.send_button} alt='send' />
       </div>
     </div>
   )
 }
 
-export default chatBox
+export default ChatBox
