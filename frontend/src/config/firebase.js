a5c0998fde7126900293ec027b11403c079dfe75
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-30 23:15:57 +0530

    firebase config

---
+++ frontend/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2TyHn9qhOrsFygFoDDc_EQmzNi8Khs7Y",
+  authDomain: "chatter-da61b.firebaseapp.com",
+  projectId: "chatter-da61b",
+  storageBucket: "chatter-da61b.firebasestorage.app",
+  messagingSenderId: "364899181836",
+  appId: "1:364899181836:web:d3bf2876b00cc01ca49529"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
