bcd00fd1d0ec7a8a3432b10eb7e436abdd9586e2
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-30 21:49:36 +0530

    profileUpdate page

--- frontend/src/pages/profileUpdate/profileUpdate.jsx
+++ frontend/src/pages/profileUpdate/profileUpdate.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './profileUpdate.css'
+import assets from "../../assets/assets"
 
 const profileUpdate = () => {
   return (
-    <div>
-        pp
+    <div className-='profile'>
+        <div className='profile-container'>
+              <form>
+                <h3>Profile Details</h3>
+                <label htmlFor="avatar">
+                  <input type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden/>
+                  <img src={assets.avatar_icon} alt=''/>
+                  upload profile image
+                </label>
+                <input type='text' placeholder='Your Name' required/>
+                <textarea placeholder='Write Profile Bio'required></textarea>
+                <button type='submit'>Save</button>
+              </form>
+              <img src={assets.logo_icon}/>
         </div>
+      </div>
   )
 }
 
