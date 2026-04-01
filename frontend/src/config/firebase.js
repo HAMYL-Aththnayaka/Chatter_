a9b8fd4c214c189630c80b732a128da7935e46da
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-04-01 13:38:33 +0530

    Firebase configuration

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,15 +1,40 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"; // Added createUser...
+import { getFirestore, doc, setDoc } from "firebase/firestore"; // Added doc, setDoc
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2TyHn9qhOrsFygFoDDc_EQmzNi8Khs7Y",
-  authDomain: "chatter-da61b.firebaseapp.com",
-  projectId: "chatter-da61b",
-  storageBucket: "chatter-da61b.firebasestorage.app",
-  messagingSenderId: "364899181836",
-  appId: "1:364899181836:web:d3bf2876b00cc01ca49529"
+  apiKey: "AIzaSyCVSVMY90SC3x-ahMF6S5DPb8Qzx0PHDFI",
+  authDomain: "chatter-b56b2.firebaseapp.com",
+  projectId: "chatter-b56b2",
+  storageBucket: "chatter-b56b2.firebasestorage.app",
+  messagingSenderId: "180363363002",
+  appId: "1:180363363002:web:5a49a9ca692208fa1a7883"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+  try {
+    // 1. Create the user in Firebase Auth
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+
+    // 2. Store additional user data in Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name:'',
+      avatar:"",
+      bio:'Hi Im a Chatter ',
+      lastSeen:Date.now(),
+    });
+    await setDoc(doc(db, "chats", user.uid), {}); // Initialize empty userChats document
+
+    console.log("User registered successfully!");
+  } catch (error) {
+    console.error("Signup error:", error.code, error.message);
+  }
+};
